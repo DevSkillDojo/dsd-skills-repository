e3f3c98c0f437fb6238da537058ab633421b7815
Greg Fullard <gfullard@gmail.com>
2020-10-10 16:18:16 +0200

    Initial structure for the skills repository page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" />
-    <Container>
-      <h1>Welcome to DevSkillDojo</h1>
+    <Container fluid className="px-0">
+    <div className="welcome-header">
+      <h1 className="display-4">Welcome to DevSkillDojo</h1>
+      <p className="lead mt-4 mb-5">Helping passionate people build world-class technical teams</p>
+    </div>
     </Container>
   </Layout>
 )
--- src/pages/repository.js
+++ src/pages/repository.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Row, Col, Container } from "react-bootstrap"
+import { Row, Col, Container, Form, Button } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,18 +7,42 @@ const Repository = () => (
   <Layout pageInfo={{ pageName: "repository" }}>
     <SEO title="Repository" />
     <div className="hero-wrapper py-5 bg-dark">
-        <div className="hero">
-          <h1 className="hero-header display-2 mb-5">Skills Repository</h1>
-          <p>The DevSkillDojo skills repository helps everyone build a better understanding of the skills involved in software development.</p>
-          <p>The focus is primarily on understanding relationships between skills</p>
-        </div>
+      <div className="hero">
+        <h1 className="hero-header display-2 mb-5">Skills Repository</h1>
+        <p>The DevSkillDojo skills repository helps everyone build a better understanding of the skills involved in software development.</p>
+        <p>The focus is primarily on understanding relationships between skills</p>
       </div>
-      <Container>
-        <Row className="pt-5 my-3">
-          <Col className="col-12 col-md-8">
-          </Col>
-        </Row>
-      </Container>
+    </div>
+    <Container>
+      <Row className="pt-5 my-3">
+        <Col className="col-12 col-md-8">
+        <h2>Search the Skills Repository</h2>
+        <Form>
+          <Form.Group controlId="formSkillSearch">
+            <Form.Control type="text" />
+          </Form.Group>
+          <Button className="bg-secondary border-secondary" variant="primary" type="submit">
+            Search
+          </Button>
+        </Form>
+        </Col>
+        <Col className="col-12 col-md-4">
+          <h3>Suggested Skills</h3>
+          <p>Skill 1</p>
+          <p>Skill 2</p>
+          <p>Skill 3</p>
+          <p>Skill 4</p>
+          <p>Skill 5</p>
+          <hr />
+          <h3>Recently Viewed</h3>
+          <p>Skill 1</p>
+          <p>Skill 2</p>
+          <p>Skill 3</p>
+          <p>Skill 4</p>
+          <p>Skill 5</p>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
