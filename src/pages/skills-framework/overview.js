05db7261b2587729c45a24280e0fde33ed195eb5
Greg Fullard <gfullard@gmail.com>
2020-12-01 06:55:32 +0200

    Text content for skills framework moved across

--- src/pages/skills-framework/overview.js
+++ src/pages/skills-framework/overview.js
@@ -2,12 +2,48 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import { Container } from "react-bootstrap"
 
 const SkillsFrameworkOverview = () => (
   <Layout>
     <SEO title="Skills Framework Overview" />
-    <h1>Skills Framework</h1>
-    <p>TODO</p>
+    <Container className="px-5"> 
+      <h1 className="mt-5 mb-1">Skills Framework</h1>
+      <p>Release Date: 2020-03-07</p>
+      <p>Version: 0.0.3</p>
+      <p>The diagram below provides a high level depiction of the DevSkillDojo Skills Framework. 
+        The intention of this framework is to provide a considered method for structuring skills. 
+        Additionally, the framework serves as a guideline to prevent one-dimensional thinking w.r.t. skills, 
+        i.e. always equating skills with technologies.</p>
+
+      <p>Key Point for understanding the purpose of the framework: 
+        Projects and products are delivered by people with capabilities. 
+        The dictionary definition of "Capability" is roughly:</p>
+
+      <blockquote>
+        <p>The ability to do [something]</p>
+      </blockquote>
+
+      <p>But it is important to give more structure to this definition, so we use the following pattern:</p>
+
+      <p>A CAPABILITY is generally:</p>
+      <ul>
+        <li>The application of one or more HARD SKILLS,</li>
+        <li>Guided by pre-existing underlying KNOWLEDGE</li>
+        <li>Using one or more TOOLS or TECHNOLOGIES</li>
+        <li>By a person with a set of SOFT SKILLS</li>
+      </ul>
+
+      <p>TODO - IMAGE</p>
+
+      <h3>Diagram Notes:</h3>
+      <ul>
+        <li>Domains are derived from the North American Industry Classification System (NAICS), but with specific adjustments to align with the goals of this frameworkFurther detail of the framework is provided in the domain model and skills ontology (documented separately)</li>
+      </ul>
+
+      <p>Further detail of the framework is provided in the domain model and skills ontology (Not published yet)</p>
+
+    </Container>
   </Layout>
 )
 
--- src/pages/skills-framework/skills-domain-model.js
+++ src/pages/skills-framework/skills-domain-model.js
@@ -2,12 +2,23 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import { Container } from "react-bootstrap"
 
 const SkillsDomainModel = () => (
   <Layout>
     <SEO title="Skills Domain Model" />
-    <h1>Skills Domain Model</h1>
-    <p>TODO</p>
+    <Container className="px-5"> 
+      <h1 className="mt-5 mb-1">Skills Domain Model</h1>
+      <p>Release Date: 2020-04-10</p>
+      <p>Version: 0.0.5</p>
+      <p>The skills domain model serves as a structural definition of all the concepts related to the skills domain. 
+        It helps us to define each concept as well as how each concept is related to others. 
+        Effectively, it is the underpinning data model of the DSD skills framework.</p>
+
+      <p>The UML diagram below provides a visual depiction of the domain model:</p>
+
+      <p>TODO - IMAGE</p>
+    </Container>
   </Layout>
 )
 
