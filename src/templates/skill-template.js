3f82c7acc221fe1de6ac384258771dec9f356735
Greg Fullard <gfullard@gmail.com>
2021-01-06 14:35:56 +0200

    Adding levels to the front-matter for skills pages

--- src/templates/skill-template.js
+++ src/templates/skill-template.js
@@ -8,7 +8,32 @@ import { Container, Row, Col, Image } from "react-bootstrap"
 const shortcodes = { Link } // Provide list of components that should be made available to MDX files here
 
 const SkillTemplate = props => {
-  console.log("Skill Template props: ", props)
+  console.log("Skill Template props: ", props);
+
+  // TODO: Deriving the levels from the front-matter. 
+  // In case of inherited levels, we need to make an API call
+  let levelDefs = {
+    type: "",
+    level1: "",
+    level2: "",
+    level3: "",
+    level4: "",
+    level5: ""
+  }
+
+  if (props.data.mdx.frontmatter.level_spec_type === "CUSTOM") {
+    levelDefs.type = "Custom";
+    levelDefs.level1 = props.data.mdx.frontmatter.levels.level1;
+    levelDefs.level2 = props.data.mdx.frontmatter.levels.level2;
+    levelDefs.level3 = props.data.mdx.frontmatter.levels.level3;
+    levelDefs.level4 = props.data.mdx.frontmatter.levels.level4;
+    levelDefs.level5 = props.data.mdx.frontmatter.levels.level5;
+  } else if (props.data.mdx.frontmatter.level_spec_type === "INHERITED") {
+    levelDefs.type = "Inherited";
+    // TODO - Make an API call
+  }
+
+
   return (
     <Layout>
       <Container>
@@ -21,9 +46,24 @@ const SkillTemplate = props => {
             <ul>
               {props.data.mdx.frontmatter.references.map((ref, i) => {
                 console.log("Found a reference: "+ref.name);
-                return (<li key={ref.name}><a href={ref.url}>{ref.name}</a></li>)})
+                return (<li key={ref.name}><a href={ref.url} target="_blank">{ref.name}</a></li>)})
               } 
             </ul>
+            <h2>Certifications:</h2>
+            <ul>
+              {props.data.mdx.frontmatter.certifications.map((cert, i) => {
+                console.log("Found a certification: "+cert.name);
+                return (<li key={cert.name}><a href={cert.url} target="_blank">{cert.name}</a></li>)})
+              } 
+            </ul>
+            <h2>Proficiency Levels ({levelDefs.type}): </h2>
+            <ul>
+              <li>Level 1: {levelDefs.level1}</li>
+              <li>Level 2: {levelDefs.level2}</li>
+              <li>Level 3: {levelDefs.level3}</li>
+              <li>Level 4: {levelDefs.level4}</li>
+              <li>Level 5: {levelDefs.level5}</li>
+            </ul>            
            </Col>
           <Col className="skill-page-header-right" sm={2}>
             {props.data.mdx.frontmatter.logo && <Image src={props.data.mdx.frontmatter.logo} alt="logo" fluid/>}
@@ -65,6 +105,18 @@ export const pageQuery = graphql`
         references {
           name,
           url
+        },
+        certifications {
+          name,
+          url
+        },
+        level_spec_type,
+        levels {
+          level1,
+          level2,
+          level3,
+          level4,
+          level5
         }
       }
     }
